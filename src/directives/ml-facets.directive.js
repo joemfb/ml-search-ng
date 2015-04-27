da5148e7ef29050d915a506a5cb367d3a5a352bd
Joseph Bryan <joemfb@gmail.com>
2015-04-27 03:45:43 -0400

    fixes truncate filter offset, adds truncate attribute to ml-facets (re #16)

--- src/directives/ml-facets.directive.js
+++ src/directives/ml-facets.directive.js
@@ -11,6 +11,7 @@
    * - `toggle`: a reference to a function that will select or clear facets based on their state. Invoked with `facet` (name) and `value` parameters. This function should invoke `mlSearch.toggleFacet(facetName, value).search()`
    * - `showMore`: a reference to a function that will pull down the next five facets. This is invoked with the `facet` itself and the `facetName`. This function should by default invoke `mlSearch.showMoreFacets(facet, facetName)`
    * - `template`: optional. A URL referencing a template to be used with the directive. If empty, the default bootstrap template will be used (chiclet-style facets). If `"inline"`, a bootstrap/font-awesome template will be used (inline facet selections)
+   * - `truncate`: optional. The length at which to truncate the facet display. Defaults to `20`.
    *
    * Example:
    *
@@ -30,7 +31,8 @@
         toggle: '&',
         showMore: '&'
       },
-      templateUrl: template
+      templateUrl: template,
+      link: link
     };
   }
 
@@ -49,6 +51,10 @@
     }
 
     return url;
+  }
+
+  function link($scope, element, attrs) {
+    $scope.truncateLength = parseInt(attrs.truncate) || 20;
   }
 
 }());
