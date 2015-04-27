412dfbe6b37adcdf670770aa45acadbbf5daf98f
Joseph Bryan <joemfb@gmail.com>
2015-04-27 03:14:31 -0400

    removes ngRoute dependency from MLRemoteInputService, replaced with best-effort (attempts to inject, fails gracefully) (re #17)

---
+++ test/spec/ml-remote-input.service-route.js
@@ -0,0 +1,43 @@
+/* global describe, beforeEach, module, it, expect, inject */
+
+describe('MLRemoteInput', function () {
+  'use strict';
+
+  var remoteInput;
+
+  beforeEach(module('ml.search'));
+  beforeEach(module('ngRoute'));
+
+  beforeEach(inject(function ($injector) {
+    remoteInput = $injector.get('MLRemoteInputService');
+  }));
+
+  it('service to be injectable with ngRoute', function() {
+    expect(remoteInput).toBeDefined;
+  });
+
+  it('callbacks to be called', function() {
+    var counter = 0,
+        latest = null,
+        unsubscribe = null;
+
+    unsubscribe = remoteInput.subscribe(function(input) {
+      latest = input;
+      counter++;
+    });
+
+    remoteInput.setInput('a');
+    expect(counter).toEqual(1);
+    expect(latest).toEqual('a');
+
+    remoteInput.setInput('b');
+    expect(counter).toEqual(2);
+    expect(latest).toEqual('b');
+
+    unsubscribe();
+
+    remoteInput.setInput('c');
+    expect(counter).toEqual(2);
+    expect(latest).toEqual('b');
+  });
+});
