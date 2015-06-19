eeed18519fd17e448f9e9055234ae4c0e7c62a5a
Joseph Bryan <joemfb@gmail.com>
2015-06-19 15:23:22 -0400

    removes `truncate` and `object2Array` filters (now in joemfb/ml-common-ng@8ae8e9e1d)

--- src/ml-search.js
+++ src/ml-search.js
@@ -1,31 +1,6 @@
 (function () {
-
   'use strict';
 
   angular.module('ml.search', ['ml.common'])
-    .filter('object2Array', object2Array)
-    .filter('truncate', truncate);
-
-  function object2Array() {
-    return function(input) {
-      var out = [];
-      for (var name in input) {
-        input[name].__key = name;
-        out.push(input[name]);
-      }
-      return out;
-    };
-  }
-
-  function truncate() {
-    return function (text, length, end) {
-      length = length || 10;
-      end = end || '...';
-
-      return (text.length > length) ?
-             String(text).substring(0, length - end.length) + end :
-             text;
-    };
-  }
 
 }());
